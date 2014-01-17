e3fbf81fda6cc76104033ea626326df44f0ec721
tik0 <korthals.timo@gmail.com>
2014-01-17 14:06:33 +0100

    The client is now able to chat with two different server. One is set by rsb.conf, the other programaticaly

--- client_PC/main.cxx
+++ client_PC/main.cxx
@@ -25,35 +25,72 @@ void printMessage(rsb::EventPtr event) {
 int main(int argc, char *argv[]) {
     string nick = "client_PC";
 
-    rsb::ParticipantConfig config;
-    rsc::runtime::Properties prop = config.getOptions();
+//    rsb::ge
+//    rsb::ParticipantConfig config;
+//    rsc::runtime::Properties prop = config.getOptions();
 
 
 
 //    std::cout << prop << std::endl;
 //    config.setOptions(prop);
 
-    const std::string& transportName = "test";
-    config.Transport transport(transportName);
+//    const std::string& transportName = "test";
+//    config.Transport transport(transportName);
 
-    config.setOptions()
+//    config.setOptions()
 
-    transport.printContents(std::cout);
+//    transport.printContents(std::cout);
 
 //    transport.setOptions()
 
 //    std::set<Transport> transports = config.getTransports(true);
 //    transports
 
-    std::cout << config.getTransports() << std::endl;
+//    std::cout << config.getTransports() << std::endl;
 
     rsb::Factory &factory = rsb::Factory::getInstance();
 
+    // Get the global participant config as a template
+    rsb::ParticipantConfig tmpPartConf = factory.getDefaultParticipantConfig();
+
+    // Let us view the default config set by "rsb.conf"
+//    std::cout << tmpPartConf <<  std::endl;
+
+    // Get the options for socket transport, because we want to change them
+    rsc::runtime::Properties tmpProp  = tmpPartConf.mutableTransport("socket").getOptions();
+
+    // Let us view the default properties for socket transport set by "rsb.conf"
+//    std::cout << tmpProp <<  std::endl;
+
+    // Get the different options
+//    std::string enabled = boost::any_cast<std::string>(tmpProp["enabled"]);
+//    std::string host    = boost::any_cast<std::string>(tmpProp["host"]);
+//    std::string port    = boost::any_cast<std::string>(tmpProp["port"]);
+//    std::string server  = boost::any_cast<std::string>(tmpProp["server"]);
+
+    // Change the config
+    std::string host = "129.70.144.5";
+    tmpProp["host"] = boost::any(host);
+
+    // Write the socket tranport properties back to the participant config
+    tmpPartConf.mutableTransport("socket").setOptions(tmpProp);
+
+    // Set the default config (We dont want to change this, because it is set by "rsb.conf")
+//    factory.setDefaultParticipantConfig()
+
+    // Set the local chatter (it uses the "rsb.conf")
     rsb::Informer<string>::Ptr informer
         = factory.createInformer<string>("/chat/text/" + nick);
     rsb::ListenerPtr listener = factory.createListener("/chat/text");
     listener->addFilter(rsb::filter::FilterPtr(new rsb::filter::OriginFilter(informer->getId(), true)));
     listener->addHandler(rsb::HandlerPtr(new rsb::EventFunctionHandler(&printMessage)));
+
+    // Set the remote chatter (it uses the programatical solution)
+    rsb::Informer<string>::Ptr informerRemote
+		= factory.createInformer<string>("/chat/text/" + nick, tmpPartConf);
+    rsb::ListenerPtr listenerRemote = factory.createListener("/chat/text", tmpPartConf);
+    listenerRemote->addFilter(rsb::filter::FilterPtr(new rsb::filter::OriginFilter(informer->getId(), true)));
+    listenerRemote->addHandler(rsb::HandlerPtr(new rsb::EventFunctionHandler(&printMessage)));
 
 
     while (true) {
@@ -65,6 +102,7 @@ int main(int argc, char *argv[]) {
             break;
         }
         informer->publish(message);
+        informerRemote->publish(message);
     }
 
     return EXIT_SUCCESS;
